2648c2de4650b70ac2ec019f2776555995d2f590
Danna Dotson <ddotson102@aol.com>
2016-01-26 20:47:10 -0600

    Guessing game modifications made v1

---
+++ script.js
@@ -0,0 +1,6 @@
+var compnumb = Math.floor((Math.random()*50) + 1);
+var number = prompt("What is your number?")
+if (number < compnumb)
+{console.log("Your number is smaller than the computers number");}
+else 
+{console.log("Your number is greater than or equal to the computers number");}
